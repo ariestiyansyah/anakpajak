cbe1563b93a6b5631a7a86de9acaa5f20d2a5ec8
Aji Achmad Mustofa <micro280991@gmail.com>
2014-06-07 16:56:28 +0700

    add new

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,23 @@
 class Question < ActiveRecord::Base
+  has_many    :answers
+  belongs_to  :user
+  has_many    :question_tags
+  has_many    :tags, through: :question_tags
+  paginates_per 12
+  acts_as_votable
+
+  def vote_up user
+    vote_by voter: user, vote: 'like'
+  end
+
+  def vote_down user
+    vote_by voter: user, vote: 'bad'
+  end
+
+  def total_vote
+    get_upvotes.size - get_downvotes.size
+  end
+
+  include PublicActivity::Model
+  tracked
 end
