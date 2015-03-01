7447e16fd1e6b6d66f870b6c755703de436248e9
Aji Achmad Mustofa <muslim272@yahoo.com>
2015-03-02 05:20:21 +0700

    Clean View

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115194048) do
+ActiveRecord::Schema.define(version: 20150301201444) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -208,6 +208,7 @@ ActiveRecord::Schema.define(version: 20141115194048) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.text     "description"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
